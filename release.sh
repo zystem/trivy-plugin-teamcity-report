b4485a0567d8c8019a9377dfaa7d0fac9b0c83d7
andrii.zagriadskyi <azystem@gmail.com>
2026-09-10 20:39:11 +0300

    Port shell scripts to POSIX sh and release 0.7.3

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-set +euo pipefail # Exit on any non-zero exit code, and error on use of undefined var
+set -eu # Exit on any non-zero exit code, and error on use of undefined var
 rm -rf ./dist && mkdir -p ./dist
 version=$(
   awk '/version:/ {
@@ -12,4 +12,4 @@ dist_filename="trivy-plugin-teamcity-report-$version.tar.gz"
 echo "Version read from plugin.yaml: $version"
 tar -czvf "./dist/$dist_filename" export_teamcity_messages.py html.tpl teamcity-report
 echo "Release archive created at dist/$dist_filename"
-gh release create "$version" --generate-notes "./dist/$dist_filename"
+#gh release create "$version" --generate-notes "./dist/$dist_filename"
